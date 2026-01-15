4089dda3ff3169a2cefb78cf4e59b01f56eda75a
Jamshid Choriyev <jamddd111000@gmail.com>
2026-01-15 19:37:01 +0500

    .

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,6 +6,7 @@ from my_app import views
 urlpatterns = [
     path("", views.home_page_view, name="home_page"),
     path("articles/", views.article_list_view, name="article_list"),
+    path("articles/<int:pk>/", views.article_detail_view, name="article_detail"),
 ]
 
 
